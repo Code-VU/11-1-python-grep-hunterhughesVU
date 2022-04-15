85245c3ce4e4f715a27d360db125e6c46bce22c7
Hunter Hughes <Hunter.Hughes@vu.com>
2022-04-15 11:05:22 -0500

    initial commit

--- grep.py
+++ grep.py
@@ -1,10 +1,29 @@
+from fileinput import filename
 import re
 import string
 import sys
 
 
 def countpatterninfile():
-    regular_expression = input("Enter a regular expression: ")
+    regular_expression = str(input("Enter a regular expression: "))
+    count = 0
+    fileName = "mbox-long.txt"
+    f = open(fileName,"r")
+    for line in f:
+        line = line.rstrip()
+        #print(line)
+        if(re.search(regular_expression,line)):
+            count += 1
+            
+    print(fileName,"had",count,"lines that matched",regular_expression)
+
+
+
+
+
+
+    
+
 
 if __name__ == '__main__':
     # this is called a main method
